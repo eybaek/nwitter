11d3cd60bdddfde0c9364f711f27fc43f28044bc
eybaek <eybaek22@gmail.com>
2021-04-25 15:37:09 +0900

    added user info
    - 누가 메세지를 작성했는지를 구분하기위해, nweet 내용이 db에 추가될 때 입력한 userId정보를 함께 저장하도록 한다.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
-const Home = () => {
-  const [nweet, setNweet] = useState("");
-  const [nweets, setNweets] = useState([]);
+const Home = ({ userObj }) => {
+const [nweet, setNweet] = useState("");
+const [nweets, setNweets] = useState([]);
   
   const getNweets = async() => {
-    const dbNweets = dbService.collection("nweets").get()
-    console.log(dbNweets);
+    const dbNweets = await dbService.collection("nweets").get()
+    dbNweets.forEach((document) => {
+      const nweetObj = {
+        ...document.data(),
+        id: document.id,
+      }
+      setNweets((prev) => [nweetObj, ...prev]);
+    })
   };
   useEffect(() => {
     getNweets();
@@ -16,9 +22,9 @@ const Home = () => {
   const onSubmit = async(event) =>{
     event.preventDefault();
     await dbService.collection("nweets").add({
-      nweet,
-      createdAt: Date.now(),
-
+      text : nweet,
+      createdAt : Date.now(),
+      creatorId : userObj.uid
     });
     setNweet('');
   }
@@ -28,6 +34,7 @@ const Home = () => {
     } = event;
     setNweet(value);
   } 
+  // console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -40,6 +47,13 @@ const Home = () => {
         />
         <input type="submit" value="nweet"/>
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
