275e0c17c837feeab1d2c7f5ecdbcf8854a6df18
eybaek <eybaek22@gmail.com>
2021-04-25 15:03:54 +0900

    메세지 작성 기능 추가.
    - 사용자가 내용 입력 후 submit을 통해 fireBase의 DataBase로 해당 내용이 저장되는 기능 추가.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  
+  const getNweets = async() => {
+    const dbNweets = dbService.collection("nweets").get()
+    console.log(dbNweets);
+  };
+  useEffect(() => {
+    getNweets();
+  }, [])
+
+  const onSubmit = async(event) =>{
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+
+    });
+    setNweet('');
+  }
+  const onChange = (event) => {
+    const {
+      target : { value }
+    } = event;
+    setNweet(value);
+  } 
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={nweet} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="What's on your mind?" 
+          maxLength={120}
+        />
+        <input type="submit" value="nweet"/>
+      </form>
+    </div>
+  )
+}
 export default Home;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,15 @@
 import { authService } from "fbase";
 import React, { Profiler } from "react";
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
 
 export default () => {
+  const history = useHistory();
   const onLogOutClick = () => {
-    const history = useHistory();
     authService.signOut();
     history.push('/');
   }
-  return <><button onClick={onLogOutClick}>Log Out</button></>;
+  return 
+  <>
+    <button onClick={onLogOutClick}>Log Out</button>
+  </>;
 };
