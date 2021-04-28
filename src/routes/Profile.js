93e31737d406255426627cdfc7c1ffaef6cdb752
eybaek <eybaek@humaxdigital.com>
2021-04-28 10:57:38 +0900

    - 계정별 트윗 내용 가져오는 기능 추가
    - (Profile 내) 트윗 내용 가져오는 기능 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,18 +1,31 @@
-import { authService } from "fbase";
-import React, { Profiler } from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   }
 
+  const getMyNweets = async() => {
+    const nweets = await dbService
+                        .collection("nweets")
+                        .where("creatorId", "==", userObj.uid)
+                        .orderBy("createdAt")
+                        .get();
+    console.log(nweets.docs.map(doc => doc.data()));
+  }
+
+  useEffect(() => {
+    getMyNweets();
+  },[])
+
   return ( 
     <div>
       <button onClick={onLogOutClick}>Log Out</button>
-      </div>
+    </div>
   )
 };
 
